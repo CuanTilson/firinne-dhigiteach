ea4fdc3c64cc759d81c031ce9c125fc960147e66
Cúán Tilson <cuantilson@gmail.com>
2026-02-10 19:28:57 +0000

    feat: add audit log and settings pages to app routing and navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,14 +5,22 @@ import { UploadPage } from "./pages/UploadPage";
 import { HistoryPage } from "./pages/HistoryPage";
 import { DetailPage } from "./pages/DetailPage";
 import { VideoDetailPage } from "./pages/VideoDetailPage";
+import { PrintDetailPage } from "./pages/PrintDetailPage";
+import { PrintVideoPage } from "./pages/PrintVideoPage";
+import { AuditLogPage } from "./pages/AuditLogPage";
+import { SettingsPage } from "./pages/SettingsPage";
 
 const App: React.FC = () => {
   return (
     <HashRouter>
       <Routes>
+        <Route path="/print/records/:id" element={<PrintDetailPage />} />
+        <Route path="/print/videos/:id" element={<PrintVideoPage />} />
         <Route path="/" element={<Layout />}>
           <Route index element={<UploadPage />} />
           <Route path="history" element={<HistoryPage />} />
+          <Route path="audit" element={<AuditLogPage />} />
+          <Route path="settings" element={<SettingsPage />} />
           <Route path="records/:id" element={<DetailPage />} />
           <Route path="videos/:id" element={<VideoDetailPage />} />
 
