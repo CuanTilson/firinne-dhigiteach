2e63f81ca9254c04ad6d9d39866564bdef497cdb
Cúán Tilson <cuantilson@gmail.com>
2026-03-01 16:58:56 +0000

    feat: add audio detail, history and print pages and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,10 +5,13 @@ import { UploadPage } from "./pages/UploadPage";
 import { HistoryPage } from "./pages/HistoryPage";
 import { DetailPage } from "./pages/DetailPage";
 import { VideoDetailPage } from "./pages/VideoDetailPage";
+import { AudioDetailPage } from "./pages/AudioDetailPage";
 import { PrintDetailPage } from "./pages/PrintDetailPage";
 import { PrintVideoPage } from "./pages/PrintVideoPage";
+import { PrintAudioPage } from "./pages/PrintAudioPage";
 import { AuditLogPage } from "./pages/AuditLogPage";
 import { SettingsPage } from "./pages/SettingsPage";
+import { AudioHistoryPage } from "./pages/AudioHistoryPage";
 
 const App: React.FC = () => {
   return (
@@ -16,13 +19,16 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/print/records/:id" element={<PrintDetailPage />} />
         <Route path="/print/videos/:id" element={<PrintVideoPage />} />
+        <Route path="/print/audio/:id" element={<PrintAudioPage />} />
         <Route path="/" element={<Layout />}>
           <Route index element={<UploadPage />} />
           <Route path="history" element={<HistoryPage />} />
+          <Route path="audio-history" element={<AudioHistoryPage />} />
           <Route path="audit" element={<AuditLogPage />} />
           <Route path="settings" element={<SettingsPage />} />
           <Route path="records/:id" element={<DetailPage />} />
           <Route path="videos/:id" element={<VideoDetailPage />} />
+          <Route path="audio/:id" element={<AudioDetailPage />} />
 
           {/* Redirect unknown routes */}
           <Route path="*" element={<Navigate to="/" replace />} />
